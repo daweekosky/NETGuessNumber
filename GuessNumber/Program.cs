97a2c09d39b21478b1beb8dddcd6179c2319f138
daweekosky <dawid_czarnecki@o2.pl>
2024-02-07 10:57:42 +0100

    Made GuessNumber App with tries counter

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿var rand = new Random();
+var value = rand.Next(1, 101);int guess = 0;int triesCount = 0;Console.WriteLine("Welcome to Guess the Number! Try to guess the number in the fewest number of tries");while(guess != value)
+{
+    Console.WriteLine("Insert your number");
+    guess = Convert.ToInt32(Console.ReadLine());
+    triesCount++;
+    if (guess == value)
+    {
+        Console.WriteLine("Congratulations! You did it in " + triesCount + " tries");
+    }
+    else if(guess < value)
+    {
+        Console.WriteLine("The number your looking for is larger");
+    }
+    else if (guess > value)
+    {
+        Console.WriteLine("The number your looking for is smaller");
+    }
+}
